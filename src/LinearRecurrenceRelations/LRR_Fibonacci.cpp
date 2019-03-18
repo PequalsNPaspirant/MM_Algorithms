bef20830df7bf2911bea1b045c81cec47d64347e
PequalsNPaspirant <marutimhetre@gmail.com>
2019-03-17 22:13:49 -0400

    Implemented different methods to calculate fibonnacci number

--- src/LinearRecurrenceRelations/LRR_Fibonacci.cpp
+++ src/LinearRecurrenceRelations/LRR_Fibonacci.cpp
@@ -1,4 +1,4 @@
-#include <vector>
+﻿#include <vector>
 #include <limits>
 using namespace std;
 
@@ -14,6 +14,28 @@ http://www.askamathematician.com/2011/04/q-is-there-a-formula-to-find-the-nth-te
 */
 
 namespace mm {
+
+	unsigned long long fibonacci_naive_v1(const unsigned long long index)
+	{
+		if (index == 0) return 0; // first term in Fibonacci sequence
+		if (index == 1) return 1; // second term in Fibonacci sequence
+		return fibonacci_naive_v1(index - 1) + fibonacci_naive_v1(index - 2);
+	}
+
+	/*
+	Time Complexity: O(n)
+	*/
+	unsigned long long fibonacci_DP_topdown_v1(const unsigned long long index)
+	{
+		static vector<unsigned long long> cache{0, 1}; //First two terms in the Fibonacci sequence
+		if (cache.size() <= index)
+			cache.resize(index + 1, 0);
+
+		if(index > 1 && cache[index] == 0)
+			cache[index] = fibonacci_DP_topdown_v1(index - 1) + fibonacci_DP_topdown_v1(index - 2);
+
+		return cache[index];
+	}
 
 	/*
 	DP bottom up
@@ -120,11 +142,173 @@ namespace mm {
 		//return t[1][1]; //We can also return t[1][1]
 	}
 
+	/*
+	Instead of above method, we can also use following direct formula
+	| 0 1 | ^ n = | f(n - 1)  f(n)     |
+	| 1 1 |       | f(n)      f(n + 1) |
+	*/
+	unsigned long long fibonacci_usingLinearRecurrence_v2(const unsigned long long index)
+	{
+		if (index == 0) return 0; // first term in Fibonacci sequence
+		if (index == 1) return 1; // second term in Fibonacci sequence
 
+		//intermediate matrix to store powers of 2
+		unsigned long long i[2][2] = {
+			0ull, 1ull,
+			1ull, 1ull
+		};
 
+		//resultant transformation matrix, initialised with unit matrix
+		unsigned long long t[2][2] = {
+			1ull, 0ull,
+			0ull, 1ull
+		};
 
+		for (unsigned int power = index - 1; power > 0; power /= 2)
+		{
+			//General idea
+			//express power as binary number. power = a 2^0 + b 2^1 + c 2^2 + ...
+			//                                      = a + 2b + 4c + ...
+			//a, b, c, ... can be either 0 or 1
+			//x ^ power = x ^ a  *  (x^2) ^ b  *  (x^4) ^ c  *  ...
 
+			if (power & 1u)
+			{
+				//t = t * i;
+				//create a temp matrix
+				unsigned long long p[2][2] = {
+					t[0][0], t[0][1],
+					t[1][0], t[1][1]
+				};
+				t[0][0] = p[0][0] * i[0][0] + p[0][1] * i[1][0];   t[0][1] = p[0][0] * i[0][1] + p[0][1] * i[1][1];
+				t[1][0] = p[1][0] * i[0][0] + p[1][1] * i[1][0];   t[1][1] = p[1][0] * i[0][1] + p[1][1] * i[1][1];
+			}
 
+			//i = i ^ 2 = i * i//Do square
+			//create a temp matrix
+			unsigned long long p[2][2] = {
+				i[0][0], i[0][1],
+				i[1][0], i[1][1]
+			};
+			i[0][0] = p[0][0] * p[0][0] + p[0][1] * p[1][0];   i[0][1] = p[0][0] * p[0][1] + p[0][1] * p[1][1];
+			i[1][0] = p[1][0] * p[0][0] + p[1][1] * p[1][0];   i[1][1] = p[1][0] * p[0][1] + p[1][1] * p[1][1];
+		}
+
+		return t[1][1];
+	}
+
+	/*
+	We know:
+
+	| 0 1 | ^ n = | f(n - 1)  f(n)     |
+	| 1 1 |       | f(n)      f(n + 1) |
+
+	Taking determinant on both sides, we get
+	(-1)^n = f(n + 1) * f(n - 1) - f(n) ^ 2
+
+	Moreover, since A^n * A^m = A^(n+m) for any square matrix A, the following identities can be derived (they are obtained form two different coefficients of the matrix product)
+
+	f(m) * f(n) + f(m-1) * f(n-1) = f(m+n-1)
+
+	if n = n+1,
+
+	f(m) * f(n+1) + f(m-1) * f(n) = f(m+n)
+
+	if m = n
+
+	f(2n-1) = f(n)^2 + f(n-1)^2
+
+	f(2n) = [f(n-1) + f(n+1)] * f(n) 
+	      = [2 * f(n-1) + f(n)] * f(n)
+
+	Summary:
+
+	f(2n)     = [2 * f(n-1) + f(n)] * f(n)
+	f(2n - 1) = f(n)^2  +  f(n - 1)^2
+
+	OR
+
+	If n is even then
+	f(n) = [2 * f(k-1) + f(k)] * f(k)     ...where k = n/2
+
+	If n is odd then 
+	f(n) = f(k)^2 + f(k-1)^2          ...where k = (n + 1)/2
+
+	*/
+	unsigned long long fibonacci_fast_recursion_topdown_v1(const unsigned long long index)
+	{
+		//if (index == 0) return 0; // first term in Fibonacci sequence
+		//if (index == 1) return 1; // second term in Fibonacci sequence
+
+		static vector<unsigned long long> cache{ 0, 1 }; //First two terms in the Fibonacci sequence
+		if (cache.size() <= index)
+			cache.resize(index + 1, 0);
+
+		if (index > 1 && cache[index] == 0)
+		{
+			if(index & 1)
+			{
+				unsigned long long k = (index + 1) / 2;
+				cache[index] = fibonacci_fast_recursion_topdown_v1(k) * fibonacci_fast_recursion_topdown_v1(k) + fibonacci_fast_recursion_topdown_v1(k - 1) * fibonacci_fast_recursion_topdown_v1(k - 1);
+			}
+			else
+			{
+				unsigned long long k = index / 2;
+				cache[index] = (2 * fibonacci_fast_recursion_topdown_v1(k - 1) + fibonacci_fast_recursion_topdown_v1(k)) * fibonacci_fast_recursion_topdown_v1(k);
+			}
+		}
+
+		return cache[index];
+	}
+
+	unsigned long long fibonacci_fast_recursion_bottomup_v1(const unsigned long long index)
+	{
+		//TODO: implement bottom up approach based on above formula
+		return 0;
+	}
+
+	/*
+	f(n) = {[(√5 + 1)/2] ^ n} / √5
+	References:
+	https://en.wikipedia.org/wiki/Fibonacci_number
+	http://www.maths.surrey.ac.uk/hosted-sites/R.Knott/Fibonacci/fibFormula.html
+	*/
+	unsigned long long fibonacci_Binet_v1(const unsigned long long index)
+	{
+		const static double phi = (1.0 + sqrt(5.0)) / 2.0;
+		//return round(pow(phi, index) / sqrt(5.0));
+		return floor(pow(phi, index) / sqrt(5.0) + 0.5);
+	}
+
+	/*
+	There is a closed-form expression for the n-th Fibonacci number, Binet's formula (http://en.wikipedia.org/wiki/Binet%27s_formula),
+
+	F(n) = (φ^n - ψ^n) / (φ - ψ)
+
+	where
+
+	φ = (1 + √5)/2;  ψ = 1 - φ = -1/φ
+	Now |ψ| < 1, so the ψ^n term converges to 0 pretty fast, hence in estimating the size of F(n) it can be ignored except for the first few numbers.
+
+	So if you have an integer type with b bits used for the representation of positive integers, you can represent the Fibonacci numbers with
+
+	F(n) < 2^b
+	(since the maximal number that can be represented is 2^b - 1). Ignoring the ψ^n term and using φ - ψ = √5, we find the condition
+
+		φ^n < 2^b * √5
+	<=> n*log φ < b*log 2 + 1/2*log 5
+	<=> n < b*(log 2 / log φ) + 1/2*(log 5 / log φ)
+	log 2 / log φ ≈ 1.44042009 and 1/2*(log 5 / log φ) ≈ 1.672275938, so with a signed 32-bit integer type (which has 31 bits to represent positive numbers, since one bit is used for the sign), you can represent the Fibonacci numbers for
+
+	n < 31*(log 2 / log φ) + 1/2*(log 5 / log φ) ≈ 44.65 + 1.67 ≈ 46.32
+	i.e. the 47 Fibonacci numbers with index between 0 and 46 (inclusive). With an unsigned 32-bit integer type you could also represent F(47).
+
+	With a signed 64-bit integer type, you can represent the Fibonacci numbers for
+
+	n < 63*(log 2 / log φ) + 1/2*(log 5 / log φ) ≈ 90.75 + 1.67 ≈ 92.42
+	and with an unsigned 64-bit integer type You can also represent F(93)
+	
+	*/
 
 	//Testing
 	MM_DECLARE_FLAG(LLR_Fibonacci)
@@ -236,12 +420,16 @@ namespace mm {
 			unsigned long long expectedValue = data[index].second;
 			unsigned long long actualValue = 0;
 
-			//MM_EXPECT_TRUE((actualValue = fibonacci_naive_v1(data[index].first)) == expectedValue, data[index].first, actualValue, expectedValue);
-			//MM_EXPECT_TRUE((actualValue = fibonacci_fast_recursion_v1(data[index].first)) == expectedValue, data[index].first, actualValue, expectedValue);
-			//MM_EXPECT_TRUE((actualValue = fibonacci_DP_topdown_v1(data[index].first)) == expectedValue, data[index].first, actualValue, expectedValue);
+			if(index < 30) //Avoid expensive recursion for big index
+			MM_EXPECT_TRUE((actualValue = fibonacci_naive_v1(data[index].first)) == expectedValue, data[index].first, actualValue, expectedValue);
+			
+			MM_EXPECT_TRUE((actualValue = fibonacci_DP_topdown_v1(data[index].first)) == expectedValue, data[index].first, actualValue, expectedValue);
 			MM_EXPECT_TRUE((actualValue = fibonacci_DP_bottomup_v1(data[index].first)) == expectedValue, data[index].first, actualValue, expectedValue);
 			MM_EXPECT_TRUE((actualValue = fibonacci_usingLinearRecurrence_v1(data[index].first)) == expectedValue, data[index].first, actualValue, expectedValue);
-			//MM_EXPECT_TRUE((actualValue = fibonacci_Binet_v1(data[index].first)) == expectedValue, data[index].first, actualValue, expectedValue);
+			MM_EXPECT_TRUE((actualValue = fibonacci_usingLinearRecurrence_v2(data[index].first)) == expectedValue, data[index].first, actualValue, expectedValue);
+			MM_EXPECT_TRUE((actualValue = fibonacci_fast_recursion_topdown_v1(data[index].first)) == expectedValue, data[index].first, actualValue, expectedValue);
+			//MM_EXPECT_TRUE((actualValue = fibonacci_fast_recursion_bottomup_v1(data[index].first)) == expectedValue, data[index].first, actualValue, expectedValue);
+			MM_EXPECT_TRUE((actualValue = fibonacci_Binet_v1(data[index].first)) == expectedValue, data[index].first, actualValue, expectedValue);
 		}
 	}
 
